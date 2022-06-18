97b6daab615d8017da710e67dea26b94280a7f81
77den <77denisov@gmail.com>
2022-06-18 09:46:44 +0300

    ListPeoplesFragment.kt ListPeoplesViewModel.kt

---
+++ app/src/main/java/ru/dw/starvars/presenter/list/ListPeoplesFragment.kt
@@ -0,0 +1,44 @@
+package ru.dw.starvars.presenter.list
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.dw.starvars.databinding.FragmentListPeoplsBinding
+
+class ListPeoplesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ListPeoplesFragment()
+    }
+    private var _binding:FragmentListPeoplsBinding? = null
+    private val binding get() = _binding!!
+
+    private  val viewModel: ListPeoplesViewModel by lazy {
+        ViewModelProvider(this)[ListPeoplesViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListPeoplsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+
+}
---
+++ app/src/main/java/ru/dw/starvars/presenter/list/ListPeoplesViewModel.kt
@@ -0,0 +1,7 @@
+package ru.dw.starvars.presenter.list
+
+import androidx.lifecycle.ViewModel
+
+class ListPeoplesViewModel : ViewModel() {
+
+}
